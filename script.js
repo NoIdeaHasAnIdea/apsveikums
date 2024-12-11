3c338f2cdd0f709de1d30caba241d96bf1f1f331
NoIdeaHasAnIdea <mrnoname1144@gmail.com>
2024-12-11 10:24:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.querySelector("button")
+
+button.addEventListener("mouseover", () =>{
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley',
+    'blank')
+})
